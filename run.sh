d7ef70be71b2a9de284c7e01370284168b4b96e2
AMearns <amearns08@gmail.com>
2026-02-17 13:26:19 +0000

    Fix container display access

--- run.sh
+++ run.sh
@@ -8,6 +8,9 @@ docker run -it --rm \
     -v $(pwd)/ros2_ws:/ros2_ws \
     -v rosdep-cache:/home/ros/.ros \
     -v colcon-cache:/home/ros/.colcon \
+    -e DISPLAY=$DISPLAY \
+    -e QT_X11_NO_MITSHM=1 \
+    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
     --name ros2-dev \
     ros2-dev \
     /bin/bash
